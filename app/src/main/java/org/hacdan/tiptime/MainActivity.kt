5345a50798c004d8f3806c77bc46c3a817ad3020
HacDan <david.mcclellan.p@gmail.com>
2022-06-28 21:38:04 -0400

    Converted to ViewBindings. Finished logging. Logging is currently excessive for the sake of it.

--- app/src/main/java/org/hacdan/tiptime/MainActivity.kt
+++ app/src/main/java/org/hacdan/tiptime/MainActivity.kt
@@ -4,34 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
-import androidx.appcompat.widget.SwitchCompat
+import org.hacdan.tiptime.databinding.ActivityMainBinding
 import kotlin.String
 
-// TODO: Convert to View Bindings
-
-private val TAG = "MainActivity"
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.v(TAG, "Entered onCreate")
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btnCalculate = findViewById<Button>(R.id.btn_calculate)
-        val tvTipAmount = findViewById<TextView>(R.id.tv_tip_amount)
-        val etCostOfService = findViewById<EditText>(R.id.et_cost_of_service)
-        val tvTotal = findViewById<TextView>(R.id.tv_total)
-        val rgTipOptions = findViewById<RadioGroup>(R.id.rg_tip_options)
-
-        btnCalculate.setOnClickListener{
-            if (etCostOfService.text.isEmpty()) {
+        binding.btnCalculate.setOnClickListener{
+            if (binding.etCostOfService.text?.isEmpty() as Boolean) {
                 Toast.makeText(this@MainActivity, "Please enter a cost of service before calculating", Toast.LENGTH_SHORT).show()
                 Log.v(TAG, "No cost of service entered")
-            } else if (rgTipOptions.checkedRadioButtonId == -1){
+            } else if (binding.rgTipOptions.checkedRadioButtonId == -1){
                 Toast.makeText(this@MainActivity, "Please be sure to select how the service was.", Toast.LENGTH_SHORT).show()
                 Log.v(TAG, "No tip percentage was entered")
             } else {
-                val costOfService = etCostOfService.text.toString().toDouble()
+                val costOfService = binding.etCostOfService.text.toString().toDouble()
                 val tip = calculateTip()
                 Log.v(TAG, "Tip amount: $tip")
 
@@ -43,33 +38,28 @@ class MainActivity : AppCompatActivity() {
                         "%.2f",
                         tip
                     )
-                }""".also { tvTipAmount.text = it }
+                }""".also { binding.tvTipAmount.text = it }
                 """${"$"}${
                     String.format(
                         "%.2f",
                         total
                     )
-                }""".also { tvTotal.text = it }
+                }""".also { binding.tvTotal.text = it }
             }
         }
     }
 
     private fun calculateTip(): Double {
-        val rgTipOptions = findViewById<RadioGroup>(R.id.rg_tip_options)
-        val etCostOfService = findViewById<EditText>(R.id.et_cost_of_service)
-        val swRoundTip = findViewById<SwitchCompat>(R.id.sw_round_up)
-        val roundTip = swRoundTip.isChecked
-
         var tipPercent = 0.0
 
-        when(rgTipOptions.checkedRadioButtonId) {
+        when(binding.rgTipOptions.checkedRadioButtonId) {
             R.id.rb_amazing -> tipPercent = 0.2
             R.id.rb_good -> tipPercent = 0.18
             R.id.rb_okay -> tipPercent = 0.15
         }
-        val tip = etCostOfService.text.toString().toDouble() * tipPercent
+        val tip = binding.etCostOfService.text.toString().toDouble() * tipPercent
 
-        if (roundTip) {
+        if (binding.swRoundUp.isChecked) {
             return kotlin.math.ceil(tip)
         }
         return tip
